0a6493c533adc0aad73d03f8903d7c596c78622c
John Hayes <johnnyhayes7@gmail.com>
2019-05-02 17:31:17 +0000

    Automatically backed up by Learn

--- app/models/super_hero.rb
+++ app/models/super_hero.rb
@@ -2,7 +2,10 @@ class Superhero
   attr_accessor :name, :power, :bio
   
   @@all = []
-  def initialize()
-    
+  def initialize(hero_hash)
+    @name = hero_hash[:name]
+    @power = hero_hash[:power]
+    @bio = hero_hash[:bio]
+    @@all << self
   end
 end
