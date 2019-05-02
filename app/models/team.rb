d40ffe3ecf451828c627a71a9accf83a66e02062
John Hayes <johnnyhayes7@gmail.com>
2019-05-02 17:37:08 +0000

    Automatically backed up by Learn

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,8 +1,8 @@
 class Team
   attr_accessor :name, :motto
   
-  def initialize(name, motto)
-    @name = name
-    @motto = motto
+  def initialize(team_hash)
+    @name = team[:name]
+    @motto = team[:motto]
   end
 end
